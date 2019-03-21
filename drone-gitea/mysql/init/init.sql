63b9f317fd22e5dcc305d318daae1471204ee122
custertian <custertian@gmail.com>
2019-03-21 17:19:10 +0800

    test

--- drone-gitea/mysql/init/init.sql
+++ drone-gitea/mysql/init/init.sql
@@ -1,4 +1,4 @@
 use mysql;
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'Value&1234';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';
 create database gitea;
 use gitea;
