d084e38e2e41f2602f58e0c33ff2bd4ecf625b66
custertian <custertian@users.noreply.github.com>
2019-03-22 11:43:21 +0800

    v0.01

--- drone-gitea/mysql/init/init.sql
+++ drone-gitea/mysql/init/init.sql
@@ -1,4 +1,8 @@
 use mysql;
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';
-create database gitea;
-use gitea;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root_password';
+CREATE USER 'drone'@'%' IDENTIFIED BY 'drone_password';
+GRANT All privileges ON *.* TO 'drone'@'%' IDENTIFIED BY 'drone_password';
+flush privileges;
+set global log_bin_trust_function_creators = 1；
+create database drone;
+use drone;
